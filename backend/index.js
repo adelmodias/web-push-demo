95a66b7a17f326112029279db2dee3be4435f757
Adelmo Dias <adelmodias@outlook.com>
2020-09-09 16:05:22 -0300

    Testing push notification in cross browser

--- backend/index.js
+++ backend/index.js
@@ -1,28 +1,44 @@
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-
+const webpush = require("web-push");
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
-
 const port = 4000;
-
 app.get("/", (req, res) => res.send("Hello World!"));
-
 const dummyDb = { subscription: null }; //dummy in memory store
-
-const saveToDatabase = async subscription => {
-  // Since this is a demo app, I am going to save this in a dummy in memory store. Do not do this in your apps.
-  // Here you should be writing your db logic to save it.
-  dummyDb.subscription = subscription;
+const saveToDatabase = async (subscription) => {
+    // Since this is a demo app, I am going to save this in a dummy in memory store. Do not do this in your apps.
+    // Here you should be writing your db logic to save it.
+    dummyDb.subscription = subscription;
 };
-
 // The new /save-subscription endpoint
 app.post("/save-subscription", async (req, res) => {
-  const subscription = req.body;
-  await saveToDatabase(subscription); //Method to save the subscription to Database
-  res.json({ message: "success" });
+    const subscription = req.body;
+    await saveToDatabase(subscription); //Method to save the subscription to Database
+    res.json({ message: "success" });
 });
-
+const vapidKeys = {
+    publicKey:
+        "BON4u5RkRIHQ42ZRujsCYTeiFUkf87JgdH8l1i7qagdCGl0UYnacOUO6GRw2z7Q9YDK1oxkIHCSxGhxjs94OfPQ",
+    privateKey: "_TOOdMg-GPqpDMcE7S9876yYv8DWhQr3Iqmc6s3bOrw",
+};
+//setting our previously generated VAPID keys
+webpush.setVapidDetails(
+    "mailto:myuserid@email.com",
+    vapidKeys.publicKey,
+    vapidKeys.privateKey
+);
+//function to send the notification to the subscribed device
+const sendNotification = (subscription, dataToSend) => {
+    webpush.sendNotification(subscription, dataToSend);
+};
+//route to test send notification
+app.get("/send-notification", (req, res) => {
+    const subscription = dummyDb.subscription; //get subscription from your databse here.
+    const message = "Novo <b>arquivo</b> disponível!";
+    sendNotification(subscription, message);
+    res.json({ message: "message sent!!!" });
+});
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
--- frontend/service.js
+++ frontend/service.js
@@ -1,49 +1,56 @@
 // urlB64ToUint8Array is a magic function that will encode the base64 public key
 // to Array buffer which is needed by the subscription option
-const urlB64ToUint8Array = base64String => {
-  const padding = "=".repeat((4 - (base64String.length % 4)) % 4);
-  const base64 = (base64String + padding)
-    .replace(/\-/g, "+")
-    .replace(/_/g, "/");
-  const rawData = atob(base64);
-  const outputArray = new Uint8Array(rawData.length);
-  for (let i = 0; i < rawData.length; ++i) {
-    outputArray[i] = rawData.charCodeAt(i);
-  }
-  return outputArray;
+const urlB64ToUint8Array = (base64String) => {
+    const padding = "=".repeat((4 - (base64String.length % 4)) % 4);
+    const base64 = (base64String + padding)
+        .replace(/\-/g, "+")
+        .replace(/_/g, "/");
+    const rawData = atob(base64);
+    const outputArray = new Uint8Array(rawData.length);
+    for (let i = 0; i < rawData.length; ++i) {
+        outputArray[i] = rawData.charCodeAt(i);
+    }
+    return outputArray;
 };
-
-const saveSubscription = async subscription => {
-  const SERVER_URL = "http://localhost:4000/save-subscription";
-  const response = await fetch(SERVER_URL, {
-    method: "post",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify(subscription)
-  });
-  return response.json();
+const saveSubscription = async (subscription) => {
+    const SERVER_URL = "http://localhost:4000/save-subscription";
+    const response = await fetch(SERVER_URL, {
+        method: "post",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(subscription),
+    });
+    return response.json();
 };
-
-self.addEventListener("install", async () => {
-  // This will be called only once when the service worker is installed for first time.
-  try {
-    const applicationServerKey = urlB64ToUint8Array(
-      "BJ5IxJBWdeqFDJTvrZ4wNRu7UY2XigDXjgiUBYEYVXDudxhEs0ReOJRBcBHsPYgZ5dyV8VjyqzbQKS8V7bUAglk"
-    );
-    const options = { applicationServerKey, userVisibleOnly: true };
-    const subscription = await self.registration.pushManager.subscribe(options);
-    const response = await saveSubscription(subscription);
-    console.log(response);
-  } catch (err) {
-    console.log("Error", err);
-  }
+self.addEventListener("activate", async () => {
+    // This will be called only once when the service worker is installed for first time.
+    try {
+        const applicationServerKey = urlB64ToUint8Array(
+            "BON4u5RkRIHQ42ZRujsCYTeiFUkf87JgdH8l1i7qagdCGl0UYnacOUO6GRw2z7Q9YDK1oxkIHCSxGhxjs94OfPQ"
+        );
+        const options = { applicationServerKey, userVisibleOnly: true };
+        const subscription = await self.registration.pushManager.subscribe(
+            options
+        );
+        const response = await saveSubscription(subscription);
+        console.log(response);
+    } catch (err) {
+        console.log("Error", err);
+    }
 });
-
-self.addEventListener("push", function(event) {
-  if (event.data) {
-    console.log("Push event!! ", event.data.text());
-  } else {
-    console.log("Push event but no data");
-  }
+self.addEventListener("push", function (event) {
+    if (event.data) {
+        console.log("Push event!! ", event.data.text());
+        showLocalNotification("Yolo", event.data.text(), self.registration);
+    } else {
+        console.log("Push event but no data");
+    }
 });
+const showLocalNotification = (title, body, swRegistration) => {
+    const options = {
+        body,
+        // here you can add more properties like icon, image, vibrate, etc.
+    };
+    swRegistration.showNotification(title, options);
+};
